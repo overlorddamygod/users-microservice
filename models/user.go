e6659c8f7eaeae525f075ede6bf7f6dce0323566
overlorddamygod <overlord.damygod@gmail.com>
2024-05-08 11:08:10 +0545

    test

--- models/user.go
+++ models/user.go
@@ -10,3 +10,10 @@ type User struct {
 	Name  string `binding:"required,min=3"`
 	Email string `binding:"required,email"`
 }
+
+type UserModel struct {
+	gorm.Model
+	ID    int32  `gorm:"primaryKey;autoIncrement"`
+	Name  string `binding:"required,min=3"`
+	Email string `binding:"required,email"`
+}
